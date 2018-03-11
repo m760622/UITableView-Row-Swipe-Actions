a1a6c39e6861f004c93bdec40805016222328ce4
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-11 05:27:11 +0400

    Added star images to tell if item is favorite or not.

--- UITableView Row Swipe Actions/Model/CustomCell.swift
+++ UITableView Row Swipe Actions/Model/CustomCell.swift
@@ -6,9 +6,15 @@ import UIKit
 class CustomCell: UITableViewCell {
     
     @IBOutlet weak var item_name: UILabel!
+    @IBOutlet weak var star_image: UIImageView!
     
     func configureCell(_ item: Item){
         item_name.text = item.item_name
+        if item.is_favorite == true {
+            star_image.image = #imageLiteral(resourceName: "fav")
+        } else {
+            star_image.image = #imageLiteral(resourceName: "not-fav")
+        }
     }
     
 }
